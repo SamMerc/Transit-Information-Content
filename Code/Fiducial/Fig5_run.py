2fc3bfd90a4e58c5bfd5f9c59cc3f2f0ddedd09b
SamMerc <samson.mercier18@gmail.com>
2026-01-14 11:12:02 -0500

    2026-01-14 - Minor update

--- Code/Fiducial/Fig5_run.py
+++ Code/Fiducial/Fig5_run.py
@@ -4,7 +4,7 @@
 
 # Figure 5 showcases the amplification factor change across wavelength, orbital geometry, and stellar type.
 # The goal of this file is to run injection-retrievals for a grid of 1. wavelengths, 2. impact parameters, 3. stellar effective temperatures, and 4. noise seeds.
-# In particular the grid is 0.6-5.3 micron with R=300 wavelengths by 5 impact parameters by 10 stellar effective temperatures by 10 noise seeds, i.e. 350 injection-retrievals.
+# In particular the grid is 0.6-5.3 micron with R=300 wavelengths by 5 impact parameters by 10 stellar effective temperatures by 10 noise seeds, i.e. 327,500 injection-retrievals.
 # In these tests, the injected LC is done with a 4th order non-linear LDL, while the retrieval is done with polynomial LDLs going from 2-9 and 4NLLD.
 
 ######################################
@@ -27,11 +27,9 @@ import time
 import arviz as az
 import numpy as np
 import os, itertools, sys
-from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
 import numpyro
 from numpyro.distributions import Normal, Uniform
 from numpyro.infer import MCMC, NUTS, HMC, init_to_value
-from lmfit import minimize, Parameters
 
 # For multi-core parallelism (useful when running multiple MCMC chains in parallel)
 numpyro.set_host_device_count(1)
