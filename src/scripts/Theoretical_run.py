23eef7099f05e9191faaf03c1e36a20347ff47c0
SamMerc <samson.mercier18@gmail.com>
2026-07-15 13:05:13 -0400

    2026-07-15 - Remove useless files

--- src/scripts/Theoretical_run.py
+++
@@ -1,8 +0,0 @@
-#############################
-########## Purpose ##########
-#############################
-
-# Figure 5 showcases the amplification factor change across wavelength, orbital geometry, and stellar type.
-# The goal of this file is to run injection-retrievals for a grid of 1. wavelengths, 2. impact parameters, 3. stellar effective temperatures, and 4. noise seeds.
-# In particular the grid is 0.6-5.3 micron with R=300 wavelengths by 5 impact parameters by 10 stellar effective temperatures by 10 noise seeds, i.e. 327,500 injection-retrievals.
-# In these tests, the injected LC is done with a 4th order non-linear LDL, while the retrieval is done with polynomial LDLs going from 2-9 and 4NLLD.
